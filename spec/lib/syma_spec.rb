78d61e257bf59892bba25d4238f84dfb333501a7
Boy Maas <boy.maas@gmail.com>
2012-06-02 00:58:24 +0200

    added spec for syma

---
+++ spec/lib/syma_spec.rb
@@ -0,0 +1,33 @@
+require 'lib/syma'
+
+describe Syma do
+  before do
+    described_class.configure do |c|
+      c.ui_driver_class :AClass 
+      c.world :AWorld
+    end
+  end
+  context "#initialize" do
+    it "on initialization it has the class instance configuration" do
+      subject.configuration.ui_driver_class == :AClass
+      subject.configuration.world == :AWorld
+    end
+    it "should be an unique instance" do
+      subject.configuration.object_id.should_not == 
+        described_class.configuration.object_id
+    end
+  end
+
+  context "#configuration" do
+    it "has a configuration" do
+      described_class.configuration.should be_an(Syma::Configuration)
+    end
+  end
+
+  context "#configure" do
+    it "can be configured" do
+      described_class.configuration.ui_driver_class.should == :AClass
+      described_class.configuration.world.should == :AWorld
+    end
+  end
+end

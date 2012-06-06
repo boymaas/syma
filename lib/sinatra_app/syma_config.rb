ffcc60cc1a5aa6c416fbac4ea576aa55ff08ed57
Boy Maas <boy.maas@gmail.com>
2012-06-06 17:50:32 +0200

    some more refactoring using new syma functionality

--- lib/sinatra_app/syma_config.rb
+++ lib/sinatra_app/syma_config.rb
@@ -2,20 +2,7 @@ require 'syma'
 
 module SinatraApp
 
-  class GivenDriver < Syma::GivenDriver
-
-    def a_user(name)
-      user = {:email => 'bob@example.com', :password => '12345'}
-      result = SinatraApp::Simple.create_user(user)
-      mental_model.users[name] = result
-    end
-
-    def a_widget(name, attributes = {})
-      widget = {:name => 'Foo'}.merge(attributes)
-      result = SinatraApp::Simple.create_widget(widget)
-      mental_model.widgets[name] = result
-    end
-  end
+  ## SignInSreen and WidgetList interface componentes
 
   class SignInScreen < Syma::UIComponent
     component_path '/session/new'
@@ -30,9 +17,22 @@ module SinatraApp
     end
   end
 
-  class WidgetList < Syma::UIComponent
+  # WidgetScreen and the form on this screen
+
+  class WidgetForm < Syma::UIComponent
+    component_selector '#widget_form'
+
+    def submit(widget_data)
+      w.fill_in 'Name:', :with => widget_data[:name]
+      w.click_on 'Save'
+    end
+  end
+
+  class WidgetScreen < Syma::UIComponent
     component_path '/widgets'
     component_selector '#widget_list'
+
+    ui_component :form, WidgetForm
 
     def widgets
       w.all('.widget_summary').map do |el|
@@ -63,39 +63,47 @@ module SinatraApp
     end
   end
 
-  class WidgetForm < Syma::UIComponent
-    component_selector '#widget_form'
 
-    def submit(widget_data)
-      w.fill_in 'Name:', :with => widget_data[:name]
-      w.click_on 'Save'
+  # Drivers
+
+  class GivenDriver < Syma::GivenDriver
+    def a_user(name)
+      user = {:email => 'bob@example.com', :password => '12345'}
+      result = SinatraApp::Simple.create_user(user)
+      mental_model.users[name] = result
+    end
+
+    def a_widget(name, attributes = {})
+      widget = {:name => 'Foo'}.merge(attributes)
+      result = SinatraApp::Simple.create_widget(widget)
+      mental_model.widgets[name] = result
     end
   end
 
+
   class UIDriver < Syma::UIDriver
     ui_component :sign_in_screen, SignInScreen
-    ui_component :widget_list, WidgetList
-    ui_component :widget_form, WidgetForm
+    ui_component :widget_screen, WidgetScreen
 
     def sign_in(name)
       go_to sign_in_screen
       sign_in_screen.sign_in(mental_model.users[name])
     end
 
-    def view_widget_list
-      go_to widget_list
+    def view_widget_screen
+      go_to widget_screen
     end
 
     def create_new_widget(name, attributes = {})
-      raise  "Widget list is not visible!" unless widget_list.visible?
-      widget_list.choose_to_create_new_widget
-      widget_form.submit(:name => 'My Widget')
-      mental_model.widgets[name] = widget_list.last_widget_created
+      raise  "Widget list is not visible!" unless widget_screen.visible?
+      widget_screen.choose_to_create_new_widget
+      widget_screen.form.submit(:name => 'My Widget')
+      mental_model.widgets[name] = widget_screen.last_widget_created
     end
 
     def delete_widget(name)
-      raise  "Widget list is not visible!" unless widget_list.visible?
-      widget_list.choose_to_delete_widget(mental_model.widgets.delete(name))
+      raise  "Widget list is not visible!" unless widget_screen.visible?
+      widget_screen.choose_to_delete_widget(mental_model.widgets.delete(name))
     end
   end
 end
--- spec/lib/sinatra_app/syma_config_spec.rb
+++ spec/lib/sinatra_app/syma_config_spec.rb
@@ -18,6 +18,8 @@ module SinatraApp
 
     before do
       extend Syma::TestHelpers
+
+      # Create user bob and add 2 widgets
       given.a_user(:bob)
       given.a_widget(:widget_a)
       given.a_widget(:widget_b, :name => 'Foo')
@@ -27,19 +29,19 @@ module SinatraApp
 
     context "/widgets" do
       before do
-        ui.view_widget_list
+        ui.view_widget_screen
       end
       context "given: no mutations" do
         it "widget list contain widget_a and widget_b" do
-          ui.widget_list.widgets.should == mental_model.widgets.values_at(:widget_a, :widget_b)
+          ui.widget_screen.widgets.should == mental_model.widgets.values_at(:widget_a, :widget_b)
         end
         it "after creating a new widget, it is visible in the list" do
           ui.create_new_widget(:widget_c, :name => 'Bar')
-          ui.widget_list.widgets.should == mental_model.widgets.values_at(:widget_a, :widget_b, :widget_c)
+          ui.widget_screen.widgets.should == mental_model.widgets.values_at(:widget_a, :widget_b, :widget_c)
         end
         it "after deleteing a widget, it should be removed from the list" do
           ui.delete_widget(:widget_b)
-          ui.widget_list.widgets.should == mental_model.widgets.values_at(:widget_a)
+          ui.widget_screen.widgets.should == mental_model.widgets.values_at(:widget_a)
         end
       end
     end
